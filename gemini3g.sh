a2973ec1e284be954515956baadd265fb4bd5ee6
NakoTurk <filmoloji89@gmail.com>
2023-11-08 22:45:16 +0300

    Update gemini3g.sh

--- gemini3g.sh
+++ gemini3g.sh
@@ -36,7 +36,7 @@ wget https://github.com/subspace/subspace/releases/download/gemini-3g-2023-nov-0
 
 sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-07 /usr/local/bin/subspace-node
 
-sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3g-2023-nov-07 /usr/local/bin/subspace-farmer
+sudo mv subspace-farmer-ubuntu-x86_64-skylake-gemini-3g-2023-nov-07 /usr/local/bin/subspace-farmer
 
 sudo chmod +x /usr/local/bin/subspace*
 
