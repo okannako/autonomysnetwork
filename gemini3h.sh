5ee44425e920f4f6b07f01f75af179a396fa609a
NakoTurk <filmoloji89@gmail.com>
2024-05-24 12:34:10 +0300

    Update gemini3h.sh

--- gemini3h.sh
+++ gemini3h.sh
@@ -30,13 +30,13 @@ fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-wget https://github.com/subspace/subspace/releases/download/gemini-3h-2024-may-15/subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-may-15
+wget https://github.com/subspace/subspace/releases/download/gemini-3h-2024-may-24/subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-may-24
 
-wget https://github.com/subspace/subspace/releases/download/gemini-3h-2024-may-15/subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-may-15
+wget https://github.com/subspace/subspace/releases/download/gemini-3h-2024-may-24/subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-may-24
 
-sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-may-15 /usr/local/bin/subspace-node
+sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-may-24 /usr/local/bin/subspace-node
 
-sudo mv subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-may-15 /usr/local/bin/subspace-farmer
+sudo mv subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-may-24 /usr/local/bin/subspace-farmer
 
 sudo chmod +x /usr/local/bin/subspace*
 
