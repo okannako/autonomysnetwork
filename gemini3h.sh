997549ebd34ab156677444fbf16c49edba0bd62f
NakoTurk <filmoloji89@gmail.com>
2024-09-21 03:06:28 +0300

    Update gemini3h.sh

--- gemini3h.sh
+++ gemini3h.sh
@@ -30,13 +30,13 @@ fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-wget https://github.com/autonomys/subspace/releases/download/gemini-3h-2024-sep-03/subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-sep-03
+wget https://github.com/autonomys/subspace/releases/download/gemini-3h-2024-sep-17/subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-sep-17
 
-wget https://github.com/autonomys/subspace/releases/download/gemini-3h-2024-sep-03/subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-sep-03
+wget https://github.com/autonomys/subspace/releases/download/gemini-3h-2024-sep-17/subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-sep-17
 
-sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-sep-03 /usr/local/bin/subspace-node
+sudo mv subspace-node-ubuntu-x86_64-skylake-gemini-3h-2024-sep-17 /usr/local/bin/subspace-node
 
-sudo mv subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-sep-03 /usr/local/bin/subspace-farmer
+sudo mv subspace-farmer-ubuntu-x86_64-skylake-gemini-3h-2024-sep-17 /usr/local/bin/subspace-farmer
 
 sudo chmod +x /usr/local/bin/subspace*
 sudo systemctl start subspace-farmer.service
