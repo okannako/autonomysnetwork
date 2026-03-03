0b91a061ca0838838a2943ae7f8cdbdac2bfad58
NakoTurk <filmoloji89@gmail.com>
2026-03-03 03:40:26 +0300

    Update mainnet2.sh

--- mainnet2.sh
+++ mainnet2.sh
@@ -30,10 +30,10 @@ fi
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-wget https://github.com/autonomys/subspace/releases/download/mainnet-2025-dec-02/subspace-farmer-ubuntu-x86_64-v2-mainnet-2025-dec-02
-wget https://github.com/autonomys/subspace/releases/download/mainnet-2025-dec-02/subspace-node-ubuntu-x86_64-v2-mainnet-2025-dec-02
-sudo mv subspace-node-ubuntu-x86_64-v2-mainnet-2025-dec-02 /usr/local/bin/subspace-node
-sudo mv subspace-farmer-ubuntu-x86_64-v2-mainnet-2025-dec-02 /usr/local/bin/subspace-farmer
+wget https://github.com/autonomys/subspace/releases/download/mainnet-2025-dec-02/subspace-farmer-ubuntu-x86_64-v2-mainnet-2026-feb-28
+wget https://github.com/autonomys/subspace/releases/download/mainnet-2025-dec-02/subspace-node-ubuntu-x86_64-v2-mainnet-2026-feb-28
+sudo mv subspace-node-ubuntu-x86_64-v2-mainnet-2026-feb-28 /usr/local/bin/subspace-node
+sudo mv subspace-farmer-ubuntu-x86_64-v2-mainnet-2026-feb-28 /usr/local/bin/subspace-farmer
 
 sudo chmod +x /usr/local/bin/subspace*
 sudo systemctl start subspace-farmer.service
